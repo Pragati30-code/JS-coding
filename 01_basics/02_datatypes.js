73c3a712c286ca1d449d4275aaf317a2e16ef758
Pragati Panwar <145797835+Pragati30-code@users.noreply.github.com>
2024-01-28 14:54:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) //we are using nodejs not browser
+
+//code readability must be high
+
+let name = "khushi"
+let age = 19
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint for large numbers
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assigned
+//symbol => unique (used in react)
+
+//object
+
+console.log(typeof "khushi");//gives output as string
+console.log(typeof age);//gives output as number
+console.log(typeof null);//gives output as object
+console.log(typeof undefined);//gives output as undefined
+
+
