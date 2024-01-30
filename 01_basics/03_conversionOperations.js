37ff0c1d0368cca7d2482f0dd20587f2a22caf1b
Pragati Panwar <145797835+Pragati30-code@users.noreply.github.com>
2024-01-30 16:42:22 +0000

    conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,14 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //gives Nan(not a number)
+
+//"33" => 33
+//"33abc" => Nan
+//true => 1; False => 0
+//"" => false
+//"khushi" => true
+
