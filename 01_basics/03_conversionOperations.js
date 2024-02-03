d69b88c8ace7f80dbd65815fbaa5ffba134f8c6d
Pragati Panwar <145797835+Pragati30-code@users.noreply.github.com>
2024-02-03 16:41:10 +0000

    conversions done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,14 +1,44 @@
-let score = "33abc"
+/*let score = "33abc"
 
 console.log(typeof score);
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
-console.log(valueInNumber); //gives Nan(not a number)
+console.log(valueInNumber); //gives Nan(not a number)*/
 
 //"33" => 33
 //"33abc" => Nan
 //true => 1; False => 0
 //"" => false
 //"khushi" => true
+
+
+/********** OPERATIONS ********/
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+//console.log(2+2);//sum
+//console.log(2-2);//subtraction
+//console.log(2*2);//multiplication
+//console.log(2**3);//raise to the power
+//console.log(2/4);//division
+//console.log(2%3);//remainder
+
+
+//console.log("1" + 2);//gives 12
+//console.log("2" + 1);//gives 21
+//console.log("1" + 2 + 2);//gives 122
+//console.log(1 + 2 + "2");//gives 32
+
+//console.log((3 + 4) * 5 % 3);//gives 2
+
+//console.log(+true);//gives 1
+//console.log(+"");//gives 0
+
+
+
+
+
+
 
