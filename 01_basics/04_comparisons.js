d91e7b9a0bcd0a11f441bb292fd6e2fe765ee27b
Pragati Panwar <145797835+Pragati30-code@users.noreply.github.com>
2024-02-04 09:57:24 +0000

    comparisons done

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,22 @@
+//console.log(2 < 3);//gives true
+//console.log(2 <= 3);//gives true
+//console.log(2 > 3);//gives false
+//console.log(2 == 3);//gives false
+//console.log(2 != 3);//gives true
+
+//console.log("2" > 1);//gives true
+//console.log("02" > 1);//gives true
+
+//console.log(null > 0);//gives false
+//console.log(null == 0);//gives false
+//console.log(null >= 0);//gives true
+
+//console.log(undefined > 0);//gives false
+//console.log(undefined == 0);//gives false
+//console.log(undefined >= 0);//gives false
+
+//=== checks the datatypes
+//console.log("2" === 2);//gives false 
+
+
+
