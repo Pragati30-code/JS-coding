718e19e268723d755d56f026b66e4ae7706cdd6f
Pragati Panwar <145797835+Pragati30-code@users.noreply.github.com>
2024-02-04 10:08:07 +0000

    summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+//primitive datatypes
+//there are 7 types of primitive datatypes
+//string,null,number,boolean,undefined,symbol,bigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 2423993870027376728635n
+
+
+//Refrence Non primitive datatypes
+//Array,objects,functions
+
+const heroes = ["shaktiman","naagraj","doga"];
+let myObj = {
+    name: "khushi",
+    age: 20,
+}
+ const myFunctions = function(){
+    console.log("Hello World");
+ }
+
+ console.log(typeof anotherId);
+ 
