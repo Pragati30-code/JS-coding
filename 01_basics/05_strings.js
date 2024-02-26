3feef5bc294af3b9c1a401b74c36945b680af0eb
Pragati Panwar <145797835+Pragati30-code@users.noreply.github.com>
2024-02-26 16:49:20 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "khushi"
+const repoCount = 60
+
+//console.log(name + repoCount + "value");
+
+//console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+
+//const gameName = new String('Pragatipp')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
