c6e048614ad6d9bb35268ef1b4ac8ae4f67cee89
Pragati Panwar <145797835+Pragati30-code@users.noreply.github.com>
2024-02-26 17:06:43 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,9 +6,19 @@ const repoCount = 60
 //console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
 
 
-//const gameName = new String('Pragatipp')
+const gameName = new String('Pragati-pp')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
 
-console.log(gameName.length);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase()); //it will not change the original value
+
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('t'));
+
+//const newString = gameName.substring(0,4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-9,4)
+console.log(anotherString);
